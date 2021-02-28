46834d1eab1cf3e133987518c9287d4802ae3327
Patrick Sturm <siyb@geekosphere.org>
2021-02-28 20:01:56 +0100

    initial commit

---
+++ app/src/main/java/com/example/monitoringandfeedback2/StartFragment.java
@@ -0,0 +1,31 @@
+package com.example.monitoringandfeedback2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+public class StartFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_startfragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        final NavController controller = Navigation.findNavController(view);
+        view.findViewById(R.id.button).setOnClickListener(button -> {
+            controller.navigate(
+                    StartFragmentDirections
+                            .actionStartFragmentToDashboardFragment("Another Dashboard Title")
+            );
+        });
+    }
+}
